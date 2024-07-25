7d6dc351b6a334e262c62e2ac21632e0dff0f158
MattHandzel <39449480+MattHandzel@users.noreply.github.com>
2024-07-25 17:59:29 +0000

    adding data again (plz)

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ let allData = []; // To store all data after initial load
 
 const svg = d3.select("#calendar");
 const tooltip = d3.select("#tooltip");
+const tooltipText = d3.select("#tooltip-text");
+const heartRatePieChart = d3.select("#heart-rate-pie-chart");
+const timeSpentPieChart = d3.select("#time-spent-pie-chart");
 
 const parseDate = d3.timeParse("%Y-%m-%d");
 const formatDate = d3.timeFormat("%B %d, %Y");
 const formatMonthYear = d3.timeFormat("%B %Y");
+
+const pie = d3.pie().value(d => d.value);
+const arc = d3.arc().innerRadius(0).outerRadius(50);
 
 d3.json("./data.json").then(data => {
     data.forEach(d => {
@@ -21,6 +27,129 @@ d3.json("./data.json").then(data => {
 }).catch(error => {
     console.error("Error loading the JSON data:", error);
 });
+
+function updateCalendar(data, monthYear) {
+    const calendar = d3.select("#calendar");
+    calendar.selectAll("*").remove(); // Clear existing calendar
+
+    const monthName = formatMonthYear(monthYear);
+    const monthDays = d3.timeDays(d3.timeMonth(monthYear), d3.timeMonth.offset(monthYear, 1));
+
+    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const monthStartDay = monthDays[0].getDay();
+
+    calendar.append("div")
+        .attr("class", "header")
+        .attr("colspan", 7)
+        .text(monthName);
+
+    dayNames.forEach(day => {
+        calendar.append("div")
+            .attr("class", "header")
+            .text(day);
+    });
+
+    for (let i = 0; i < monthStartDay; i++) {
+        calendar.append("div")
+            .attr("class", "day empty");
+    }
+
+    monthDays.forEach(date => {
+        const dayData = data.find(d => d3.timeDay(d.date).getTime() === date.getTime());
+
+        const dayCell = calendar.append("div")
+            .attr("class", "day")
+            .text(date.getDate())
+            .on("mouseover", function(event) {
+                if (dayData) {
+                    tooltip.transition().duration(200).style("opacity", .9);
+
+                    // Update text
+                    tooltipText.html(`
+                        <strong>Date:</strong> ${formatDate(dayData.date)}<br>
+                        <strong>Sleep:</strong> ${dayData.sleep_duration} hours<br>
+                        <strong>Steps:</strong> ${dayData.num_steps}<br>
+                        <strong>Weight:</strong> ${dayData.weight} lbs<br>
+                    `);
+
+                    // Update heart rate pie chart
+                    const heartRateData = Object.keys(dayData.heart_rate_pie_chart).map(key => {
+                        return { label: key, value: dayData.heart_rate_pie_chart[key] };
+                    });
+
+                    const heartRateArcs = pie(heartRateData);
+                    const heartRateSvg = heartRatePieChart.selectAll("svg").data([null]);
+
+                    const heartRateSvgEnter = heartRateSvg.enter().append("svg")
+                        .attr("width", 100)
+                        .attr("height", 100)
+                        .append("g")
+                        .attr("transform", "translate(50,50)");
+
+                    heartRateSvgEnter.merge(heartRateSvg).selectAll("path")
+                        .data(heartRateArcs)
+                        .join("path")
+                        .attr("d", arc)
+                        .attr("fill", (d, i) => d3.schemeCategory10[i]);
+
+                    // Update time spent pie chart
+                    const timeSpentData = Object.keys(dayData.timeSpent).map(key => {
+                        return { label: key, value: dayData.timeSpent[key] };
+                    });
+
+                    const timeSpentArcs = pie(timeSpentData);
+                    const timeSpentSvg = timeSpentPieChart.selectAll("svg").data([null]);
+
+                    const timeSpentSvgEnter = timeSpentSvg.enter().append("svg")
+                        .attr("width", 100)
+                        .attr("height", 100)
+                        .append("g")
+                        .attr("transform", "translate(50,50)");
+
+                    timeSpentSvgEnter.merge(timeSpentSvg).selectAll("path")
+                        .data(timeSpentArcs)
+                        .join("path")
+                        .attr("d", arc)
+                        .attr("fill", (d, i) => d3.schemeCategory10[i]);
+
+                    tooltip.style("left", (event.pageX + 5) + "px")
+                           .style("top", (event.pageY - 28) + "px");
+                }
+            })
+            .on("mouseout", function() {
+                tooltip.transition().duration(500).style("opacity", 0);
+            });
+
+        if (dayData && dayData.events.length) {
+            dayCell.style("background-color", "lightcoral");
+        }
+    });
+}
+
+function changeMonth(offset) {
+    currentDate.setMonth(currentDate.getMonth() + offset);
+    const filteredData = allData.filter(d => d.date.getMonth() === currentDate.getMonth() && d.date.getFullYear() === currentDate.getFullYear());
+    updateCalendar(filteredData, currentDate);
+}
+
+document.getElementById("prevMonth").addEventListener("click", () => changeMonth(-1));
+document.getElementById("nextMonth").addEventListener("click", () => changeMonth(1));
+
+// Initial load
+d3.json("./data.json").then(data => {
+    data.forEach(d => {
+        d.date = parseDate(d.date);
+        d.sleep_duration = +d.sleep_duration;
+        d.num_steps = +d.num_steps;
+        d.weight = +d.weight;
+    });
+
+    allData = data; // Store all data
+    updateCalendar(data, currentDate);
+}).catch(error => {
+    console.error("Error loading the JSON data:", error);
+});
+
 allData = [
     {
         "date": "2023-08-01",
@@ -336,91 +465,4 @@ allData = [
     }
 ]
 
-function updateCalendar(data, monthYear) {
-    const calendar = d3.select("#calendar");
-    calendar.selectAll("*").remove(); // Clear existing calendar
-
-    const monthName = formatMonthYear(monthYear);
-    const monthDays = d3.timeDays(d3.timeMonth(monthYear), d3.timeMonth.offset(monthYear, 1));
-
-    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const monthStartDay = monthDays[0].getDay();
-
-    calendar.append("div")
-        .attr("class", "header")
-        .attr("colspan", 7)
-        .text(monthName);
-
-    dayNames.forEach(day => {
-        calendar.append("div")
-            .attr("class", "header")
-            .text(day);
-    });
-
-    for (let i = 0; i < monthStartDay; i++) {
-        calendar.append("div")
-            .attr("class", "day empty");
-    }
-
-    monthDays.forEach(date => {
-        const dayData = data.find(d => d3.timeDay(d.date).getTime() === date.getTime());
-
-        const dayCell = calendar.append("div")
-            .attr("class", "day")
-            .text(date.getDate())
-            .on("mouseover", function(event) {
-                if (dayData) {
-                    tooltip.transition().duration(200).style("opacity", .9);
-                    tooltip.html(`
-                        <strong>Date:</strong> ${formatDate(dayData.date)}<br>
-                        <strong>Sleep:</strong> ${dayData.sleep_duration} hours<br>
-                        <strong>Steps:</strong> ${dayData.num_steps}<br>
-                        <strong>Weight:</strong> ${dayData.weight} lbs<br>
-                        <strong>Time Spent:</strong>
-                        <ul>
-                            <li>Class: ${dayData.timeSpent.class} min</li>
-                            <li>Study: ${dayData.timeSpent.study} min</li>
-                            <li>Leisure: ${dayData.timeSpent.leisure} min</li>
-                            <li>Exercise: ${dayData.timeSpent.exercise} min</li>
-                            <li>Reading: ${dayData.timeSpent.reading} min</li>
-                            <li>Socializing: ${dayData.timeSpent.socializing} min</li>
-                            <li>Eating: ${dayData.timeSpent.eating} min</li>
-                        </ul>
-                    `)
-                    .style("left", (event.pageX + 5) + "px")
-                    .style("top", (event.pageY - 28) + "px");
-                }
-            })
-            .on("mouseout", function() {
-                tooltip.transition().duration(500).style("opacity", 0);
-            });
-
-        if (dayData && dayData.events.length) {
-            dayCell.style("background-color", "lightcoral");
-        }
-    });
-}
-
-function changeMonth(offset) {
-    currentDate.setMonth(currentDate.getMonth() + offset);
-    const filteredData = allData.filter(d => d.date.getMonth() === currentDate.getMonth() && d.date.getFullYear() === currentDate.getFullYear());
-    updateCalendar(filteredData, currentDate);
-}
-
-document.getElementById("prevMonth").addEventListener("click", () => changeMonth(-1));
-document.getElementById("nextMonth").addEventListener("click", () => changeMonth(1));
-
-// Initial load
-d3.json("./data.json").then(data => {
-    data.forEach(d => {
-        d.date = parseDate(d.date);
-        d.sleep_duration = +d.sleep_duration;
-        d.num_steps = +d.num_steps;
-        d.weight = +d.weight;
-    });
-
-    allData = data; // Store all data
-    updateCalendar(data, currentDate);
-}).catch(error => {
-    console.error("Error loading the JSON data:", error);
-});
+console.log(allData);
